8c6e91a9eb1e28ace6a64dde924b55235dc889a5
Muhammad Kamal <mahmoodkamaluddeen@gmail.com>
2023-12-23 00:34:45 +0100

    Added validation to request model

--- APIHubCore/Models/CreateUserRequest.cs
+++ APIHubCore/Models/CreateUserRequest.cs
@@ -1,6 +1,7 @@
 ﻿using Azure.Core;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,21 @@ namespace APIHubCore.Models
 {
     public class CreateUserRequest
       {
-            public string ?RequestId { get; set; }
-            public string UserId { get; set; }
-            public string? UserName { get; set; }
-            public string? UserType { get; set; }
-            public string? Password { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9 ]*$")]    
+        public string RequestId { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9 ]*$")]
+        public string UserId { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9 ]*$")]
+        public string UserName { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9 ]*$")]
+        public string  UserType { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9 ]*$")]
+        public string  Password { get; set; }
         }
     }
 
